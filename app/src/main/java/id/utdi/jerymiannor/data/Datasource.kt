18b7e53eaf747e511444c87d2689f70c614fe0f3
ZerJ704 <opbrrr@gmail.com>
2023-10-13 15:54:31 +0700

    Initial commit

---
+++ app/src/main/java/id/utdi/jerymiannor/data/Datasource.kt
@@ -0,0 +1,19 @@
+package id.utdi.jerymiannor.data
+
+import id.utdi.jerymiannor.R
+import id.utdi.jerymiannor.model.Affirmation
+
+/**
+ * [Datasource] generates a list of [Affirmation]
+ */
+class Datasource() {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2),
+            Affirmation(R.string.affirmation3, R.drawable.image3),
+            Affirmation(R.string.affirmation4, R.drawable.image4),
+            Affirmation(R.string.affirmation5, R.drawable.image5)
+        )
+    }
+}
